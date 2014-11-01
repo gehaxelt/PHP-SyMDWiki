40434823214ea7e0c9817250a76b0f6186d9c1a0
Sebastian Gehaxelt <github@gehaxelt.in>
2014-11-01 17:46:19 +0100

    Implemented firewalls and login

---
+++ src/Gehaxelt/SyMDWikiBundle/Controller/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Gehaxelt\SyMDWikiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login")
+     * @Template()
+     */
+    public function loginAction(Request $request)
+    {
+      $session = $request->getSession();
+       
+      if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) 
+      {
+          $error = $request->attributes->get(
+              SecurityContext::AUTHENTICATION_ERROR
+          );
+      }
+      else 
+      {
+          $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+          $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+      }
+       
+      return array(
+              // last username entered by the user
+              'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+              'error'         => $error,
+          );
+    }
+}
---
+++ src/Gehaxelt/SyMDWikiBundle/GehaxeltSyMDWikiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gehaxelt\SyMDWikiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GehaxeltSyMDWikiBundle extends Bundle
+{
+}
